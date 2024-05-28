52a3ac18a3301545aaed67e68b39f4968a11f618
debora berte <arobedbee@gmail.com>
2024-05-27 22:30:32 -0300

    first commit

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "A whole lotta flake to run your very own Ruby Project";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        inherit (pkgs.lib) optional optionals;
+        pkgs = import nixpkgs { inherit system; };
+
+        inputs = with pkgs;
+          [ 
+            ruby_3_3 
+            rubyPackages_3_3.rails 
+            rubyPackages_3_3.sinatra
+            glibcLocales 
+          ]
+          ++ optional stdenv.isLinux [ inotify-tools ]
+          ++ optional stdenv.isDarwin terminal-notifier
+          ++ optionals stdenv.isDarwin
+          (with darwin.apple_sdk.frameworks; [ CoreFoundation CoreServices ]);
+      in with pkgs; {
+        devShells.default = mkShell {
+          name = "starship-comm";
+          packages = inputs;
+        };
+      });
+}
