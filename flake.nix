88c5f0f50ea7c229c95dd0265006d8c07059faa8
debora berte <arobedbee@gmail.com>
2024-06-01 21:24:16 -0300

    infra: adjust Gemfile + flake.nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A whole lotta flake to run your very own Ruby Project";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -12,25 +12,45 @@
         pkgs = import nixpkgs { inherit system; };
 
         inputs = with pkgs;
-          [ 
-            ruby_3_3 
-            rubyPackages_3_3.rails 
+          [
+            ruby_3_3
+            rubyPackages_3_3.rails
             rubyPackages_3_3.sinatra
             bundler
             act
-            msgpack # explicitly add msgpack
+            msgpack
             libyaml
             zlib.dev
             libffi.dev
+            direnv
           ]
           ++ pkgs.lib.optional pkgs.stdenv.isLinux [ pkgs.inotify-tools ]
           ++ pkgs.lib.optional pkgs.stdenv.isDarwin pkgs.terminal-notifier
-          ++ pkgs.lib.optionals pkgs.stdenv.isDarwin
-          (with pkgs.darwin.apple_sdk.frameworks; [ CoreFoundation CoreServices ]);
+          ++ pkgs.lib.optionals pkgs.stdenv.isDarwin (with pkgs.darwin.apple_sdk.frameworks; [ CoreFoundation CoreServices ]);
       in {
+        packages = {
+          default = pkgs.stdenv.mkDerivation {
+            name = "starship-comm";
+            src = ./.;
+            buildInputs = inputs;
+            buildPhase = ''
+              bundle install --path vendor/bundle
+            '';
+            installPhase = ''
+              mkdir -p $out
+              cp -r . $out/
+            '';
+          };
+        };
+
         devShells.default = pkgs.mkShell {
           name = "starship-comm";
           packages = inputs;
+          shellHook = ''
+            eval "$(direnv hook bash)"
+            bundle install --path vendor/bundle
+          '';
         };
-      });
+      }
+    );
 }
