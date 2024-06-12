b194bc930169ab26b7768e123d1e4d0b16743ba1
debora berte <arobedbee@gmail.com>
2024-06-11 22:16:58 -0300

    feat: job for updating prices

---
+++ app/jobs/update_prices.rb
@@ -0,0 +1,30 @@
+class UpdatePricesJob
+    include Sidekiq::Worker
+  
+    def perform
+      Product.all.each do |product|
+        competitor_price = fetch_competitor_price(product)
+        old_price = product.price
+        product.update(
+          price: product.dynamic_price,
+          competitor_price: competitor_price
+        )
+        PriceLog.create!(
+          product: product,
+          old_price: old_price,
+          new_price: product.price,
+          changed_at: Time.now
+        )
+      end
+    end
+  
+    private
+  
+    def fetch_competitor_price(product)
+      response = Faraday.get("https://sinatra-pricing-api.fly.dev/products/#{product.id}")
+      JSON.parse(response.body)["price"]
+    rescue
+      nil
+    end
+  end
+  
