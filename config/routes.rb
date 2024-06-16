9b6073e43aae3feac60f0844307eafe77d8758f0
debora berte <arobedbee@gmail.com>
2024-06-15 23:03:16 -0300

    chore: improve CI

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   Rails.application.routes.draw do
-    resources :products, only: [:index, :show, :create, :update, :destroy]
+    resources :products
   end
   
 
