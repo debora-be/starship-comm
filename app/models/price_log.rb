bdbd7357ae8fbfd678bcb80fcf65a3732fec9566
debora berte <arobedbee@gmail.com>
2024-06-16 22:47:59 -0300

    chore: update model

--- app/models/price_log.rb
+++ app/models/price_log.rb
@@ -2,10 +2,15 @@ class PriceLog
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  field :product_id, type: BSON::ObjectId
+  field :product_id, type: Integer
   field :old_price, type: Float
   field :new_price, type: Float
   field :changed_at, type: DateTime
+
+  validates :product_id, presence: true
+  validates :new_price, presence: true, numericality: { greater_than: 0 }
+  validates :old_price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :changed_at, presence: true
 
   belongs_to :product
 end
